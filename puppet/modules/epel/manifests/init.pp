8f5509eb8ac89243d8e6bb4de0b6a9b9ab5bc5c5
Dustin Wheeler <mdwheele@ncsu.edu>
2014-02-11 10:05:12 -0500

    Bugfixes related to libmcrypt not being able to find gpg keys.

--- puppet/modules/epel/manifests/init.pp
+++ puppet/modules/epel/manifests/init.pp
@@ -4,20 +4,33 @@ class epel (
     $source = 0
 ) {
     yumrepo { "epel":
-        descr    => "Extra Packages for Enterprise Linux $operatingsystemmajrelease - \$basearch",
-        baseurl  => "ftp://ftp.linux.ncsu.edu/pub/epel/$operatingsystemmajrelease/\$basearch",
+        descr    => "Extra Packages for Enterprise Linux ${operatingsystemmajrelease} - \$basearch",
+        baseurl  => "ftp://ftp.linux.ncsu.edu/pub/epel/${operatingsystemmajrelease}/\$basearch",
         enabled  => $enabled
     }
 
     yumrepo { "epel-debuginfo":
-        descr    => "Extra Packages for Enterprise Linux $operatingsystemmajrelease - \$basearch - Debug",
-        baseurl  => "ftp://ftp.linux.ncsu.edu/pub/epel/$operatingsystemmajrelease/\$basearch/debug",
+        descr    => "Extra Packages for Enterprise Linux ${operatingsystemmajrelease} - \$basearch - Debug",
+        baseurl  => "ftp://ftp.linux.ncsu.edu/pub/epel/${operatingsystemmajrelease}/\$basearch/debug",
         enabled  => $debuginfo
     }
 
     yumrepo { "epel-source":
-        descr    => "Extra Packages for Enterprise Linux $operatingsystemmajrelease - \$basearch - Source",
-        baseurl  => "ftp://ftp.linux.ncsu.edu/pub/epel/$operatingsystemmajrelease/SRPMS",
+        descr    => "Extra Packages for Enterprise Linux ${operatingsystemmajrelease} - \$basearch - Source",
+        baseurl  => "ftp://ftp.linux.ncsu.edu/pub/epel/${operatingsystemmajrelease}/SRPMS",
         enabled  => $source
+    }
+
+    file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${$operatingsystemmajrelease}":
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => "puppet:///modules/epel/RPM-GPG-KEY-EPEL-${$operatingsystemmajrelease}",
+    }
+
+    epel::rpm_gpg_key{ "EPEL-${operatingsystemmajrelease}":
+        path    => "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-${operatingsystemmajrelease}",
+        before  => Yumrepo['epel','epel-source','epel-debuginfo'],
     }
 }
