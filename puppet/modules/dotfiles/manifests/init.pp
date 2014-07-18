d988db521b96000d94161991fdc303aa54fff1aa
Dustin Wheeler <mdwheele@ncsu.edu>
2014-07-18 00:26:56 -0400

    Bugfix.

--- puppet/modules/dotfiles/manifests/init.pp
+++ puppet/modules/dotfiles/manifests/init.pp
@@ -2,7 +2,7 @@ class dotfiles {
 
     exec { "merge_files" :
         cwd => "/home/vagrant",
-        command => 'yes | cp -R /vagrant/puppet/modules/dotfiles/files/.[!.]* ./ && for file in /vagrant/files/dotfiles/.[!.]*; do if [ $(basename $file) != ".gitignore" ]; then cp -f $(basename $file) /tmp/$(basename $file) && cat /tmp/$(basename $file) /vagrant/files/dotfiles/$(basename $file) > $(basename $file) ; fi ; done',
+        command => 'yes | cp -R /vagrant/files/dotfiles/.[!.]* ./ && cp -R /vagrant/puppet/modules/dotfiles/files/.[!.]* ./ && for file in /vagrant/files/dotfiles/.[!.]*; do if [ $(basename $file) != ".gitignore" ]; then cp -f $(basename $file) /tmp/$(basename $file) && cat /tmp/$(basename $file) /vagrant/files/dotfiles/$(basename $file) > $(basename $file) ; fi ; done',
     }
 
 }
