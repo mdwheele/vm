40ea786363a2d8a36169189ed1ddf89636d726ca
Dustin Wheeler <mdwheele@ncsu.edu>
2014-10-12 22:31:19 -0400

    Updates to librarian-puppet script.

--- scripts/run-librarian-puppet.sh
+++ scripts/run-librarian-puppet.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 if [ "$(/usr/bin/gem search -i librarian-puppet)" = "false" ]; then
+  echo "[Puppet Librarian] Installing modules..."
   cd /vagrant && librarian-puppet install --path puppet/modules-contrib
 else
-  cd /vagrant && librarian-puppet update --path puppet/modules-contrib
+  echo "[Puppet Librarian] Updating modules..."
+  cd /vagrant && librarian-puppet update
 fi
