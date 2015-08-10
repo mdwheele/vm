efafe1219ecdce0c9ac59c2cfc3a359e678efa71
Dustin Wheeler <mdwheele@ncsu.edu>
2015-08-10 15:10:52 -0400

    Fixes yum repository config.

--- puppet/modules/yum/manifests/repo/eosphp54.pp
+++ puppet/modules/yum/manifests/repo/eosphp54.pp
@@ -1,8 +1,8 @@
-class yum::repo::eostesting {
+class yum::repo::eosphp54 {
 
-    yumrepo { "eos-testing" :
+    yumrepo { "eos-php54" :
         ensure => present,
-        baseurl => "http://install.linux.ncsu.edu/pub/yum/itecs/servers/eos-RHEL6-testing",
+        baseurl => "http://install.linux.ncsu.edu/pub/yum/itecs/servers/php54",
         descr => "EOS testing repository",
         enabled => 1,
         gpgcheck => 0,
